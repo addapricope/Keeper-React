94e29168cc4cf041ff2678eece0bb2a5b0684ffd
addapricope <114485759+addapricope@users.noreply.github.com>
2022-11-28 14:03:36 +0200

    Updates

---
+++ precache-manifest.dab6b086b4c52b024845f758310e7254.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a67f746a6e241a67d5e84c0a000a1fd",
+    "url": "/Keeper-React/index.html"
+  },
+  {
+    "revision": "91a8ce9d867e3d9aab4d",
+    "url": "/Keeper-React/static/js/2.7358e7b1.chunk.js"
+  },
+  {
+    "revision": "3de1b018b84879513887",
+    "url": "/Keeper-React/static/js/main.0ca46f9d.chunk.js"
+  },
+  {
+    "revision": "88a6ce9f28d07e73a730",
+    "url": "/Keeper-React/static/js/runtime-main.f5589fcc.js"
+  }
+]);
